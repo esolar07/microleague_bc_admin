32aa5f0e53a428bd84da37719416eefc90be8b34
muhammadfaizan7986 <muhammadfaizan7986@gmail.com>
2026-04-14 12:12:44 +0500

    fixes

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -1,16 +1,17 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, sepolia } from "wagmi/chains";
+import { baseSepolia, mainnet, sepolia } from "wagmi/chains";
 import { http } from "wagmi";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "MicroLeague Presale Admin",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || "YOUR_PROJECT_ID",
-  chains: [mainnet, sepolia],
+  chains: [baseSepolia],
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    // [mainnet.id]: http(),
+    // [sepolia.id]: http(),
+    [baseSepolia.id]: http(),
   },
   ssr: false,
 });
 
-export const supportedChains = [mainnet, sepolia];
+export const supportedChains = [baseSepolia];
